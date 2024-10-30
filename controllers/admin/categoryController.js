69aebb564eafe7b405de6ba8fb51af9b523f02a2
Sameeha Ansari <sameehaansari011292@gmail.com>
2024-10-30 19:38:03 +0530

    add brand

--- controllers/admin/categoryController.js
+++ controllers/admin/categoryController.js
@@ -121,9 +121,86 @@ const addCategory= async(req,res)=>{
  }
 
 
+
+ const getListCategory = async (req,res)=>{
+    try {
+        let id = req.query.id;
+        await Category.updateOne({_id:id},{$set:{isListed:false}});
+        res.redirect("/admin/category")
+        
+    } catch (error) {
+        res.redirect("/pageerror")
+        
+    }
+ }
+
+
+ const getUnlistCategory = async(req,res)=>{
+    try {
+        let id=req.query.id;
+        await Category.updateOne({_id:id},{$set:{isListed:true}});
+        res.redirect("/admin/category")
+        
+    } catch (error) {
+        res.redirect("/pageerror")
+        
+    }
+ }
+
+
+
+ const getEditCategory = async(req,res)=>{
+    try {
+        const id = req.query.id;
+        const category = await Category.findOne({_id:id});
+        res.render("edit-category",{category:category})
+        
+    } catch (error) {
+        res.redirect("/pageerror")
+        
+    }
+ }
+
+
+ const editCategory = async(req,res)=>{
+    try {
+
+        const id =req.params.id;
+        const {categoryName,description} = req.body;
+        const existingCategory = await Category.findOne({name:categoryName,_id: {$ne:id}});
+
+        if(existingCategory){
+            return res.status(400).json({error:"Category exists please choose another name"})
+        }
+        
+      const updateCategory = await Category.findByIdAndUpdate(id,{
+        name:categoryName,
+        description:description,
+      },{new:true});
+      console.log(updateCategory)
+
+      if(updateCategory){
+        res.redirect("/admin/category")
+      }else{
+        res.status(404).json({error:"Category not found"})
+      }
+
+
+    } catch (error) {
+        res.status(500).json({error:"Internal server error"})
+        
+    }
+ }
+
+
+
 module.exports={
     categoryInfo,
     addCategory,
     addCategoryOffer,
     removeCategoryOffer,
+    getListCategory,
+    getUnlistCategory,
+    getEditCategory,
+    editCategory
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router=express.Router();
 const adminController=require("../controllers/admin/adminController");
 const customerController=require("../controllers/admin/customerController")
-const categoryController = require("../controllers/admin/categoryController")
-const {userAuth,adminAuth} = require("../middlewares/auth")
+const categoryController = require("../controllers/admin/categoryController");
+const brandController = require("../controllers/admin/brandController")
+const {userAuth,adminAuth} = require("../middlewares/auth");
+const multer = require("multer");
+const storage = require("../helpers/multer");
+const uploads = multer({storage:storage});
 
 
 router.get("/pageerror",adminController.pageerror);
@@ -28,5 +32,16 @@ router.get("/category",adminAuth,categoryController.categoryInfo)
 router.post("/addCategory",categoryController.addCategory);
 router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer);
 router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer);
+router.get("/listCategory",adminAuth,categoryController.getListCategory);
+router.get("/unlistCategory",adminAuth,categoryController.getUnlistCategory);
+router.get("/editCategory",adminAuth,categoryController.getEditCategory);
+router.post("/editCategory/:id",adminAuth,categoryController.editCategory);
+
+//Brand Management
+
+router.get("/brands",adminAuth,brandController.getBrandPage);
+router.post("/addBrand",adminAuth,uploads.single("image"),brandController.addBrand);
+
+
 
 module.exports=router;
